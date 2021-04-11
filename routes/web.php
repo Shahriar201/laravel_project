c9f6d233c84a9481116900a618d2bbc51dea58a0
shahriar201 <shahriarbrine201@gmail.com>
2021-04-11 15:47:42 +0600

    Basic template Home About US Contact Us

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'Frontend\FrontendController@index');
+Route::get('about-us', 'Frontend\FrontendController@aboutUs')->name('about.us');
+Route::get('contact-us', 'Frontend\FrontendController@contactUs')->name('contact.us');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
