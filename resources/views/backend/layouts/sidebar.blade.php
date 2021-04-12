4b98957903acc41867c6132faf2d1045371a82ca
shahriar201 <shahriarbrine201@gmail.com>
2021-04-12 13:24:28 +0600

    view backend data from database

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
         <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="" class="nav-link">
                 <i class="nav-icon fas fa-copy"></i>
                 <p>
                     Manage User
@@ -12,7 +12,7 @@
             </a>
             <ul class="nav nav-treeview">
                 <li class="nav-item">
-                    <a href="pages/layout/top-nav.html" class="nav-link">
+                    <a href="{{ route('users.view') }}" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>View User</p>
                     </a>
