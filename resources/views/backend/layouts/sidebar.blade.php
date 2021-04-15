e38fcdf09a723120bd0372a916c80dc0a7c3db38
shahriar201 <shahriarbrine201@gmail.com>
2021-04-15 14:36:02 +0600

    Logo management by user or admin

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -51,6 +51,25 @@
             </ul>
         </li>
 
+        <li class="nav-item">
+            <a href="" class="nav-link">
+                <i class="nav-icon fas fa-copy"></i>
+                <p>
+                    Manage Logo
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{ route('logos.view') }}" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>View Logo</p>
+                    </a>
+                </li>
+                
+            </ul>
+        </li>
+
     </ul>
 </nav>
 <!-- /.sidebar-menu -->
